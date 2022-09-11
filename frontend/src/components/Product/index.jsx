558b1eb0153fea4553f31e161e6c08668e0c5058
Deias <saied_am@hotmail.com>
2022-09-11 18:28:38 +0100

    using link from react router instead of a tag

--- frontend/src/components/Product/index.jsx
+++ frontend/src/components/Product/index.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
-
 import { Card } from 'react-bootstrap'
+
+import { Link } from 'react-router-dom'
+
+import Rating from '../Rating'
 
 function Product({ product }) {
     return (
         <Card className="my-3 p-3 rounded">
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img variant="top" src={product.image} />
-            </a>
+            </Link>
             <Card.Body>
-                <a href={`/product/${product._id}`}>
+                <Link to={`/product/${product._id}`}>
                     <Card.Title as="div">
                         <strong>{product.name}</strong>
                     </Card.Title>
-                    <Card.Text my-3>
-                        {product.rating} from {product.numReviews} reviews
+                    <Card.Text className="my-3" as="div">
+                        <Rating
+                            value={product.rating}
+                            text={`${product.numReviews} reviews`}
+                            color={'#f8e825'}
+                        />
                     </Card.Text>
                     <Card.Text as="h3">${product.price}</Card.Text>
-                </a>
+                </Link>
             </Card.Body>
         </Card>
     )
