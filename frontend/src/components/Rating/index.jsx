5f4a396c75ad8ab7a40b2c977138be98599f7968
Deias <saied_am@hotmail.com>
2022-09-11 18:29:08 +0100

    creating rating component to handel the rating rendering stars

---
+++ frontend/src/components/Rating/index.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+const RatingDiv = styled.div`
+    & span {
+        margin: 0.1rem;
+    }
+`
+
+export default function Rating({ value, text, color }) {
+    const i = 5
+
+    return (
+        <RatingDiv>
+            <span>
+                <i
+                    style={{ color }}
+                    className={
+                        value >= 1
+                            ? 'fa-solid fa-star'
+                            : value >= 0.5
+                            ? 'fa-solid fa-star-half-stroke'
+                            : 'fa-regular fa-star'
+                    }></i>
+            </span>
+            <span>
+                <i
+                    style={{ color }}
+                    className={
+                        value >= 2
+                            ? 'fa-solid fa-star'
+                            : value >= 1.5
+                            ? 'fa-solid fa-star-half-stroke'
+                            : 'fa-regular fa-star'
+                    }></i>
+            </span>
+            <span>
+                <i
+                    style={{ color }}
+                    className={
+                        value >= 3
+                            ? 'fa-solid fa-star'
+                            : value >= 2.5
+                            ? 'fa-solid fa-star-half-stroke'
+                            : 'fa-regular fa-star'
+                    }></i>
+            </span>
+            <span>
+                <i
+                    style={{ color }}
+                    className={
+                        value >= 4
+                            ? 'fa-solid fa-star'
+                            : value >= 3.5
+                            ? 'fa-solid fa-star-half-stroke'
+                            : 'fa-regular fa-star'
+                    }></i>
+            </span>
+            <span>
+                <i
+                    style={{ color }}
+                    className={
+                        value >= 5
+                            ? 'fa-solid fa-star'
+                            : value >= 4.5
+                            ? 'fa-solid fa-star-half-stroke'
+                            : 'fa-regular fa-star'
+                    }></i>
+            </span>
+            <span>{text && text}</span>
+        </RatingDiv>
+    )
+}
