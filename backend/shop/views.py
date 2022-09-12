3b03acc5e2899db4832d56c4f90d45aeeb60cabb
Deias <saied_am@hotmail.com>
2022-09-12 14:59:27 +0100

    Adding data and creating serializers / view for product and products

--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,7 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .products import products
+from .models import Product
+
+from .serializers import ProductSerializer
 
 
 @api_view(['GET'])
@@ -25,13 +27,18 @@ def get_routes(request):
 
 @api_view(['GET'])
 def get_products(request):
-    return Response(products)
+    products = Product.objects.all()
+    serialized_products = ProductSerializer(products, many=True)
+    return Response(serialized_products.data)
 
 
 @api_view(['GET'])
 def get_product(request, pk):
-    product = next(
-        (prod for prod in products if prod['_id'] == pk),
-        None
-    )
-    return Response(product)
+    # product = next(
+    #     (prod for prod in products if prod['_id'] == pk),
+    #     None
+    # )
+
+    product = Product.objects.get(_id=pk)
+    serialized_product = ProductSerializer(product)
+    return Response(serialized_product.data)
