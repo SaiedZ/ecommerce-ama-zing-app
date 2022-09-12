6d94c89b55e3d70e0ce0147dbe3d51923f7710bc
Deias <saied_am@hotmail.com>
2022-09-12 20:18:36 +0100

    Loader and Message components

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 
 import Product from '../../components/Product'
+import Loader from '../../components/Loader'
+import Message from '../../components/Message'
 import { listProducts } from '../../redux/actions/productActions'
 
 function HomePage() {
@@ -20,9 +22,9 @@ function HomePage() {
         <div>
             <h1>Latest products</h1>
             {loading ? (
-                <h2>Loading...</h2>
+                <Loader />
             ) : error ? (
-                <h3>{error}</h3>
+                <Message variant="danger">{error}</Message>
             ) : (
                 <Row>
                     {products.map((product) => (
