aa0a855d722ce33faa620c2da2d1f1219f3b20fa
Deias <saied_am@hotmail.com>
2022-09-12 14:12:46 +0100

    creating models

--- backend/shop/admin.py
+++ backend/shop/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Product
+from .models import (
+    Product,
+    Review,
+    Order,
+    OrderItem,
+    ShippingAddress,
+
+)
 
 
 admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
