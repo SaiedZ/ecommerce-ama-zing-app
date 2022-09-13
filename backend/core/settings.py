b5e7818359fc5d1d6806285811640d814a4af33c
Deias <saied_am@hotmail.com>
2022-09-13 13:59:56 +0100

    adding jwt token

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -45,6 +45,12 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 
