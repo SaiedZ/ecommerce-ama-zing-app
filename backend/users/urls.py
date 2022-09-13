af8b4fc38fd2ff2ac7ef160dbcb288515b8265f7
Deias <saied_am@hotmail.com>
2022-09-13 17:51:11 +0100

    adding user sealizer and also serailizer to add user info to the token when login. Also, register view, profile

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,25 +1,24 @@
 from django.urls import path
 from . import views
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
-
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(),
+    path('login/', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
+    path('', views.getUsers, name="users"),
+    path('profile/', views.getUserProfile, name="users-profile"),
+    path('register/', views.registerUser, name='register'),
 ]
 
 """
 urlpatterns = [
-    path('login/', views.MyTokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
 
     path('register/', views.registerUser, name='register'),
 
     path('profile/', views.getUserProfile, name="users-profile"),
-    path('profile/update/', views.updateUserProfile, name="user-profile-update"),
-    path('', views.getUsers, name="users"),
+    path('profile/update/',
+        views.updateUserProfile,
+        name="user-profile-update"),
+
 
     path('<str:pk>/', views.getUserById, name='user'),
 
