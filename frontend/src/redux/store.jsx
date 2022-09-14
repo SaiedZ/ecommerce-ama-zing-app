d4e32239328d0669daa52ea5e2d35bad116e73a9
Deias <saied_am@hotmail.com>
2022-09-14 10:29:27 +0100

    creating action, reducer, constants for the user login and adding this to the store and the local storage

--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -6,8 +6,8 @@
 
 import { configureStore } from '@reduxjs/toolkit'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
-
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 // const reducer = combineReducers({
 //     productList: productListReducer
 // })
@@ -16,10 +16,15 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
     ? JSON.parse(localStorage.getItem('cartItems'))
     : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null
+
 const preloadedState = {
     cart: {
         cartItems: cartItemsFromStorage
-    }
+    },
+    userLogin: { userInfo: userInfoFromStorage }
 }
 // const middleware = [thunk]
 
@@ -27,7 +32,8 @@ const store = configureStore({
     reducer: {
         productList: productListReducer,
         productDetails: productDetailsReducer,
-        cart: cartReducer
+        cart: cartReducer,
+        userLogin: userLoginReducer
     },
     preloadedState
 })
