3687e5619bfdb1f74aee44023089d513a04fee4d
Deias <saied_am@hotmail.com>
2022-09-14 11:51:58 +0100

    passing dispatch, userInfo as props from the Header to the navbar

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,9 +1,14 @@
 import NavbarCustom from './navbar'
+import { useDispatch, useSelector } from 'react-redux'
 
 function Header() {
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
+    const dispatch = useDispatch()
+
     return (
         <header>
-            <NavbarCustom />
+            <NavbarCustom userInfo={userInfo} dispatch={dispatch} />
         </header>
     )
 }
