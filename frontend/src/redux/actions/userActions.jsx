2881ea0590c7d70a41ca4c17b8f9d43202123dda
Deias <saied_am@hotmail.com>
2022-09-14 14:32:04 +0100

    creating the getUserDetails action

--- frontend/src/redux/actions/userActions.jsx
+++ frontend/src/redux/actions/userActions.jsx
@@ -102,3 +102,35 @@ export const register = (email, name, password) => async (dispatch) => {
         })
     }
 }
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: USER_DETAILS_REQUEST })
+
+        const {
+            userLogin: { userInfo }
+        } = getState()
+
+        const config = {
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.get(`/api/users/${id}/`, config)
+
+        dispatch({
+            type: USER_DETAILS_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: USER_DETAILS_FAIL,
+            payload:
+                error.response && error.response.data.detail
+                    ? error.response.data.detail
+                    : error.message
+        })
+    }
+}
