34c6a87e3481d98d7d32c291c1855aac54d55ae5
Deias <saied_am@hotmail.com>
2022-09-14 13:36:37 +0100

    creating the register page

---
+++ frontend/src/pages/Register/index.jsx
@@ -0,0 +1,114 @@
+import { useState, useEffect } from 'react'
+
+import { Link, useNavigate, useLocation } from 'react-router-dom'
+
+import { Row, Col, Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+
+import Loader from '../../components/Loader'
+import Message from '../../components/Message'
+import FormContainer from '../../components/FormContainer'
+
+import { register } from '../../redux/actions/userActions'
+
+function RegisterPage() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState('')
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const redirect = location.search ? location.search.split('=')[1] : '/'
+
+    const userRegister = useSelector((state) => state.userRegister)
+    const { error, loading, userInfo } = userRegister
+
+    useEffect(() => {
+        if (userInfo) {
+            navigate(redirect)
+        }
+    }, [navigate, userInfo, redirect])
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        if (password != confirmPassword) {
+            setMessage('Passwords do not match')
+        } else {
+            dispatch(register(email, password, name))
+        }
+    }
+
+    return (
+        <FormContainer>
+            <h1>Register</h1>
+            {message && <Message variant="danger">{message}</Message>}
+            {error && <Message variant="danger">{error}</Message>}
+            {loading && <Loader />}
+
+            <Form onSubmit={submitHandler}>
+                <Form.Group className="mb-3" controlId="name">
+                    <Form.Label>Name</Form.Label>
+
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="Enter Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="email">
+                    <Form.Label>Email Adress</Form.Label>
+
+                    <Form.Control
+                        required
+                        type="email"
+                        placeholder="Enter Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Form.Text className="text-muted">
+                        We will never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        required
+                        type="password"
+                        placeholder="Enter Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}></Form.Control>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="passwordConfirm">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                        required
+                        type="password"
+                        placeholder="Confirm Password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}></Form.Control>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Sign In
+                </Button>
+            </Form>
+
+            <Row className="py-3">
+                <Col>
+                    Have an account ?
+                    <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Sign In</Link>
+                </Col>
+            </Row>
+        </FormContainer>
+    )
+}
+
+export default RegisterPage
