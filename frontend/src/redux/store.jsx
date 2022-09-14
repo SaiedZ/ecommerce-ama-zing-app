af696a8e9f59793ef473ea30d5f9792134e027e2
Deias <saied_am@hotmail.com>
2022-09-14 14:31:33 +0100

    creating the userDetailsReducer and adding it to the store

--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -7,7 +7,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer, userDetailsReducer } from './reducers/userReducers'
 // const reducer = combineReducers({
 //     productList: productListReducer
 // })
@@ -34,7 +34,8 @@ const store = configureStore({
         productDetails: productDetailsReducer,
         cart: cartReducer,
         userLogin: userLoginReducer,
-        userRegister: userRegisterReducer
+        userRegister: userRegisterReducer,
+        userDetails: userDetailsReducer
     },
     preloadedState
 })
