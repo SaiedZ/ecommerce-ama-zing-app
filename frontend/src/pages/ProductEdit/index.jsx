c182dda9de17cb8163a834c603dbfeb256dfe59e
Deias <saied_am@hotmail.com>
2022-09-16 15:54:42 +0100

    UPDATING THE PRODUCT IS NOW ENABLED

--- frontend/src/pages/ProductEdit/index.jsx
+++ frontend/src/pages/ProductEdit/index.jsx
@@ -32,36 +32,42 @@ function ProductEditPage() {
     const productDetails = useSelector((state) => state.productDetails)
     const { error, loading, product } = productDetails
 
+    const productUpdate = useSelector((state) => state.productUpdate)
+    const { error: errorUpdate, loading: loadingUpdate, success: successUpdate } = productUpdate
+
     useEffect(() => {
-        //if (successUpdate) {
-        //     dispatch({ type: PRODUCT_UPDATE_RESET })
-        //     navigate('/admin/productlist')
-        // } else {
-        if (!product.name || product._id !== Number(productId)) {
-            dispatch(listProductDetails(productId))
+        if (successUpdate) {
+            dispatch({ type: PRODUCT_UPDATE_RESET })
+            navigate('/admin/productlist')
         } else {
-            setName(product.name)
-            setPrice(product.price)
-            setImage(product.image)
-            setBrand(product.brand)
-            setCategory(product.category)
-            setCountInStock(product.countInStock)
-            setDescription(product.description)
+            if (!product.name || product._id !== Number(productId)) {
+                dispatch(listProductDetails(productId))
+            } else {
+                setName(product.name)
+                setPrice(product.price)
+                setImage(product.image)
+                setBrand(product.brand)
+                setCategory(product.category)
+                setCountInStock(product.countInStock)
+                setDescription(product.description)
+            }
         }
-    }, [dispatch, product, productId, navigate]) // successUpdate
+    }, [dispatch, product, productId, navigate, successUpdate])
 
     const submitHandler = (e) => {
         e.preventDefault()
-        // dispatch(updateProduct({
-        //     _id: productId,
-        //     name,
-        //     price,
-        //     image,
-        //     brand,
-        //     category,
-        //     countInStock,
-        //     description
-        // }))
+        dispatch(
+            updateProduct({
+                _id: productId,
+                name,
+                price,
+                image,
+                brand,
+                category,
+                countInStock,
+                description
+            })
+        )
     }
 
     return (
@@ -70,8 +76,8 @@ function ProductEditPage() {
 
             <FormContainer>
                 <h1>Edit Product</h1>
-                {/* {loadingUpdate && <Loader />}
-                {errorUpdate && <Message variant="danger">{errorUpdate}</Message>} */}
+                {loadingUpdate && <Loader />}
+                {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
 
                 {loading ? (
                     <Loader />
