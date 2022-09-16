e8826bd083b5ad8a133b4af715ff77b6e9146433
Deias <saied_am@hotmail.com>
2022-09-16 10:14:18 +0100

    view and url to delete product

--- backend/shop/urls/product_urls.py
+++ backend/shop/urls/product_urls.py
@@ -3,5 +3,8 @@ from shop.views import product_views
 
 urlpatterns = [
     path('', product_views.get_products, name="products"),
+    path('delete/<str:pk>/',
+         product_views.deleteProduct,
+         name="product-delete"),
     path('<str:pk>/', product_views.get_product, name="product"),
 ]
--- backend/shop/views/product_views.py
+++ backend/shop/views/product_views.py
@@ -1,5 +1,7 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from shop.models import Product
 from shop.serializers import ProductSerializer
@@ -22,3 +24,12 @@ def get_product(request, pk):
     product = Product.objects.get(_id=pk)
     serialized_product = ProductSerializer(product)
     return Response(serialized_product.data)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return ('Product Deleted')
