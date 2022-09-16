fb200c569103f9052cacdb9b83a1788d20acc9c9
Deias <saied_am@hotmail.com>
2022-09-16 21:52:05 +0100

    creating the SearchBox component

---
+++ frontend/src/components/SearchBox/index.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import { useNavigate, useLocation } from 'react-router-dom'
+
+import { useSearchParams } from 'react-router-dom'
+
+function SearchBox() {
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    // console.log('hash', location.hash)
+    // console.log('pathname', location.pathname)
+    // console.log('search', location.search)
+    // let [searchParams, setSearchParams] = useSearchParams()
+
+    const [keyword, setKeyword] = useState('')
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        if (keyword) {
+            // let params = {
+            //     keyword: keyword
+            // }
+            // setSearchParams(params)
+            navigate(`/?keyword=${keyword}`) // go to the home page with the keyword in the url   &page=1
+        } else {
+            // setSearchParams()
+            navigate(location.pathname) // clean up the pathnam from any unused querysearch
+        }
+    }
+    return (
+        <Form className="d-flex mt-2" onSubmit={submitHandler}>
+            <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                onChange={(e) => setKeyword(e.target.value)}
+            />
+            <Button type="submit" variant="outline-success" className="p-2">
+                Search
+            </Button>
+        </Form>
+    )
+}
+
+export default SearchBox
