48266a72b2434ff2be2bf672103e3478b4d4c0bd
Deias <saied_am@hotmail.com>
2022-09-16 19:45:42 +0100

    adding the url for createProductReview

--- backend/shop/urls/product_urls.py
+++ backend/shop/urls/product_urls.py
@@ -7,11 +7,15 @@ urlpatterns = [
     path('create/', product_views.createProduct, name="product-create"),
     path('upload/', product_views.uploadImage, name="image-upload"),
 
-    path('update/<str:pk>/',
-         product_views.updateProduct,
-         name="product-update"),
-    path('delete/<str:pk>/',
-         product_views.deleteProduct,
-         name="product-delete"),
+
+    path('<str:pk>/reviews/', product_views.createProductReview,
+         name="create-review"),
+    # path('top/', views.getTopProducts, name='top-products'),
     path('<str:pk>/', product_views.get_product, name="product"),
+
+
+    path('update/<str:pk>/', product_views.updateProduct,
+         name="product-update"),
+    path('delete/<str:pk>/', product_views.deleteProduct,
+         name="product-delete"),
 ]
