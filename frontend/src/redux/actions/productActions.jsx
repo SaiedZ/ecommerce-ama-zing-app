6cea349ed9a7af88a3a71b73439c11ab326d8cf0
Deias <saied_am@hotmail.com>
2022-09-17 09:09:40 +0100

    listTopProducts action

--- frontend/src/redux/actions/productActions.jsx
+++ frontend/src/redux/actions/productActions.jsx
@@ -46,6 +46,27 @@ export const listProducts =
         }
     }
 
+export const listTopProducts = () => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_TOP_REQUEST })
+
+        const { data } = await axios.get(`/api/products/top/`)
+
+        dispatch({
+            type: PRODUCT_TOP_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_TOP_FAIL,
+            payload:
+                error.response && error.response.data.detail
+                    ? error.response.data.detail
+                    : error.message
+        })
+    }
+}
+
 export const listProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_DETAILS_REQUEST })
